85843b6f23bd872ebff10fb8d10824f59e569a33
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-07 19:23:29 -0300

    advanced pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -16,29 +16,50 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{
+      method: method,
+      path: path,
+      status: nil,
+      resp_body: ""
+    }
   end
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
-    %{conv | resp_body: "Bears, Lions, Tigers"}
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
-    %{conv | resp_body: "Teddy, Smokey, Paddington"}
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+  end
+
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
   def route(%{path: path} = conv) do
-    %{conv | resp_body: "No #{path} here!"}
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    case code do
+      200 -> "OK"
+      201 -> "Created"
+      401 -> "Unauthorized"
+      403 -> "Forbidden"
+      404 -> "Not Found"
+      500 -> "Internal Server Error"
+      _ -> "Unknown Status"
+    end
   end
 end
 
@@ -68,6 +89,18 @@ IO.inspect(response)
 
 request = """
 GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.inspect(response)
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
