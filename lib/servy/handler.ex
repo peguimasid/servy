385ef6642419383ddf1d8e7174d18fe535fb9a9d
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-08 13:24:15 -0300

    refactor

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,6 +3,7 @@ defmodule Servy.Handler do
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
 
   def handle(request) do
     request
@@ -46,18 +47,6 @@ defmodule Servy.Handler do
 
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "Route '#{path}' not found."}
-  end
-
-  def handle_file({:ok, content}, conv) do
-    %{conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{conv | status: 404, resp_body: "File not found"}
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   def format_response(conv) do
