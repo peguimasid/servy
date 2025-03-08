85bb4c8f5b622d5d8024e45e83f480c170e59479
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-08 12:55:21 -0300

    modules attrs

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,6 @@
 defmodule Servy.Handler do
+  @pages_path Path.expand("../../pages", __DIR__)
+
   def handle(request) do
     request
     |> parse()
@@ -45,7 +47,7 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/bears/new"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("form.html")
     |> File.read()
     |> handle_file(conv)
@@ -60,7 +62,7 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
