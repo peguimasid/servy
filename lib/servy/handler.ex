1673cbfa4a7bc1b9ccb0a5072a06939f09a777e3
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-08 12:43:22 -0300

    serving files

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -53,11 +53,26 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    %{conv | status: 200, resp_body: "contents of file"}
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
   end
 
   def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "Route '#{path}' not found."}
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   def track(%{status: 404, path: path} = conv) do
