a510c678bcd3a221356f1efb79edf5df6ef3aa71
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-09 13:05:51 -0300

    change to resp_headers

--- lib/servy/api/bear_controller.ex
+++ lib/servy/api/bear_controller.ex
@@ -6,6 +6,6 @@ defmodule Servy.Api.BearController do
       Wildthings.list_bears()
       |> Poison.encode!()
 
-    %{conv | status: 200, resp_content_type: "application/json", resp_body: json}
+    %{conv | status: 200, resp_headers: %{"Content-Type" => "application/json"}, resp_body: json}
   end
 end
--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -66,7 +66,7 @@ defmodule Servy.Handler do
   def format_response(%Conv{} = conv) do
     """
     HTTP/1.1 #{Conv.full_status(conv)}\r
-    Content-Type: #{conv.resp_content_type}\r
+    Content-Type: #{conv.resp_headers["Content-Type"]}\r
     Content-Length: #{byte_size(conv.resp_body)}\r
     \r
     #{conv.resp_body}
