7a76873ac6199d2afba37fbe767c033650021edc
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-09 10:05:01 -0300

    use eex function from file to help speed

--- lib/servy/controllers/bear_controller.ex
+++ lib/servy/controllers/bear_controller.ex
@@ -1,30 +1,20 @@
 defmodule Servy.Controllers.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
-
-  @templates_path Path.expand("templates", File.cwd!())
-
-  defp render(conv, template, bindings) do
-    content =
-      @templates_path
-      |> Path.join(template)
-      |> EEx.eval_file(bindings)
-
-    %{conv | status: 200, resp_body: content}
-  end
+  alias Servy.View.BearView
 
   def index(conv) do
     bears =
       Wildthings.list_bears()
       |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    render(conv, "index.eex", bears: bears)
+    %{conv | status: 200, resp_body: BearView.index(bears)}
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    render(conv, "show.eex", bear: bear)
+    %{conv | status: 200, resp_body: BearView.show(bear)}
   end
 
   def create(conv, %{"type" => type, "name" => name}) do
