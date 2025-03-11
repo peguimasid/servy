c24114d637006f1f0976fda882263b58e8e9d21b
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-11 20:06:51 -0300

    use httpoison in tests

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -1,27 +1,14 @@
 defmodule HttpServerTest do
   use ExUnit.Case
 
-  alias Servy.{HttpClient, HttpServer}
+  alias Servy.{HttpServer}
 
   test "should receive correct response" do
     spawn(HttpServer, :start, [4000])
 
-    request = """
-    GET /wildthings HTTP/1.1\r
-    Host: example.com\r
-    User-Agent: ExampleBrowser/1.0\r
-    Accept: */*\r
-    \r
-    """
+    {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
 
-    response = HttpClient.send_request(request)
-
-    assert response == """
-           HTTP/1.1 200 OK\r
-           Content-Type: text/html\r
-           Content-Length: 20\r
-           \r
-           Bears, Lions, Tigers
-           """
+    assert response.status_code == 200
+    assert response.body == "Bears, Lions, Tigers"
   end
 end
