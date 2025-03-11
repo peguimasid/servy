b0a3ca910976a29d98613cb8bfa2def0656e7d5a
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-11 20:12:29 -0300

    concurrent test

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -11,4 +11,26 @@ defmodule HttpServerTest do
     assert response.status_code == 200
     assert response.body == "Bears, Lions, Tigers"
   end
+
+  test "should receive correct responses" do
+    spawn(HttpServer, :start, [4000])
+
+    parent_pid = self()
+
+    max_requests = 5
+
+    for _ <- 1..max_requests do
+      spawn(fn ->
+        send(parent_pid, HTTPoison.get("http://localhost:4000/wildthings"))
+      end)
+    end
+
+    for _ <- 1..max_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
+  end
 end
