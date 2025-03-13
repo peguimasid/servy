6c2e030171e0e9ff6e48cb6c777d2f0f79bc28a0
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-13 20:54:42 -0300

    multiple requests

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -15,16 +15,21 @@ defmodule HttpServerTest do
   test "should receive correct responses" do
     spawn(HttpServer, :start, [4000])
 
-    url = "http://localhost:4000/wildthings"
+    urls = [
+      "http://localhost:4000/wildthings",
+      "http://localhost:4000/wildlife",
+      "http://localhost:4000/bears",
+      "http://localhost:4000/bears/1",
+      "http://localhost:4000/api/bears"
+    ]
 
-    1..5
-    |> Enum.map(fn _ -> Task.async(fn -> HTTPoison.get(url) end) end)
+    urls
+    |> Enum.map(&Task.async(fn -> HTTPoison.get(&1) end))
     |> Enum.map(&Task.await/1)
     |> Enum.map(&assert_successful_response/1)
   end
 
   defp assert_successful_response({:ok, response}) do
     assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
   end
 end
