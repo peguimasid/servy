3e553bccb12a367fab5eed1760d3206848dda82e
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-13 19:51:50 -0300

    replace fetch with task

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -10,7 +10,6 @@ defmodule Servy.Handler do
   alias Servy.VideoCam
   alias Servy.Api
   alias Servy.Conv
-  alias Servy.Fetcher
   alias Servy.Controllers.BearController
 
   def handle(request) do
@@ -25,14 +24,14 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/sensors"} = conv) do
-    pid4 = Fetcher.async(fn -> Tracker.get_location("bigfoot") end)
+    task = Task.async(fn -> Tracker.get_location("bigfoot") end)
 
     snapshots =
       ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&Fetcher.async(fn -> VideoCam.get_snapshot(&1) end))
-      |> Enum.map(&Fetcher.get_result/1)
+      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Task.await/1)
 
-    where_is_bigfoot = Fetcher.get_result(pid4)
+    where_is_bigfoot = Task.await(task)
 
     %{conv | status: 200, resp_body: inspect({snapshots, where_is_bigfoot})}
   end
