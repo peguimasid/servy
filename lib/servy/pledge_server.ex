ea995b4c03e44ade96cfa2ccd111055a3eb4a750
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-14 19:57:54 -0300

    add state to module via listed loop

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,7 +1,25 @@
 defmodule Servy.PledgeServer do
-  def create_pledge(name, amount) do
-    {:ok, id} = send_pledge_to_service(name, amount)
+  def listen_loop(state) do
+    IO.puts("\nWaiting for message...")
+
+    receive do
+      {:create_pledge, name, amount} ->
+        {:ok, id} = send_pledge_to_service(name, amount)
+        new_state = [{name, amount} | state]
+        IO.puts("#{name} pledged #{amount}!")
+        IO.puts("New state is #{inspect(new_state)}")
+        listen_loop(new_state)
+
+      {sender, :recent_pledges} ->
+        send(sender, {:response, state})
+        IO.puts("Send pledged to #{inspect(sender)}!")
+        listen_loop(state)
+    end
   end
+
+  # def create_pledge(name, amount) do
+  #   {:ok, id} = send_pledge_to_service(name, amount)
+  # end
 
   def recent_pledges do
     #
