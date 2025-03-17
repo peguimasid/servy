01d5eac8afeb6b079208fd35b944d9a04d3148bf
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-17 20:10:31 -0300

    get server function

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -6,10 +6,18 @@ defmodule Servy.KickStarter do
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def get_server do
+    GenServer.call(__MODULE__, :get_server)
+  end
+
   def init(:ok) do
     Process.flag(:trap_exit, true)
     server_pid = start_http_server()
     {:ok, server_pid}
+  end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
   end
 
   def handle_info({:EXIT, _pid, reason}, _state) do
